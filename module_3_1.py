bcba25263125a9d2a1ebe8a9238af3a53874415e
UvarovART <ezufir@yandex.ru>
2024-08-08 19:45:37 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -4,7 +4,7 @@ def count_calls():
     calls += 1
 def string_info(string):
     count_calls()
-    return len(string), string.upper, string.lower
+    return len(string), string.upper(), string.lower()
 def is_contains(string, list_to_search):
     count_calls()
     return string.upper() in [s.upper() for s in list_to_search]
